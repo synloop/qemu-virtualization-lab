0a1c06d46552a1c6855967fa8ead4db89c610044
synloop <209967120+synloop@users.noreply.github.com>
2025-06-15 21:28:22 +0200

    Added vm_run.sh automation script
    • 2 vCPU, 4 GB RAM, KVM enabled  
    • VirtIO disk / net + Guest Agent socket (/tmp/qga.sock)  
    • Boot from qcow2 (-d) or ISO (-i); ISO takes priority  
    • Host port 4000 → guest 22 for SSH

---
+++ src/vm_run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+RAM="4G"
+CPUS=2
+SOCK="/tmp/qga.sock"
+
+usage() {
+  echo "Usage: $0 -d <disk.qcow2> -i <iso.iso>"
+  exit 1
+}
+
+while getopts ":d:i:" opt; do
+  case $opt in
+    d) DISK="$OPTARG" ;;
+    i) ISO="$OPTARG" ;;
+    *) usage ;;
+  esac
+done
+
+if [[ -z "$DISK" && -z "$ISO" ]]; then
+  echo "Erreur : il faut au moins -d ou -i"
+  usage
+fi
+
+QEMU_OPTS="-enable-kvm -m $RAM -smp cpus=$CPUS"
+QEMU_OPTS+=" -vga virtio"
+QEMU_OPTS+=" -netdev user,id=net0,hostfwd=tcp::4000-:22"
+QEMU_OPTS+=" -device virtio-net-pci,netdev=net0"
+QEMU_OPTS+=" -device virtio-serial"
+QEMU_OPTS+=" -chardev socket,path=$SOCK,server=on,wait=off,id=qga0"
+QEMU_OPTS+=" -device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0"
+
+if [[ -n "$DISK" ]]; then
+  QEMU_OPTS+=" -drive file=$DISK,format=qcow2,if=virtio"
+fi
+
+if [[ -n "$ISO" ]]; then
+  QEMU_OPTS+=" -cdrom $ISO -boot d"
+fi
+
+qemu-system-x86_64 $QEMU_OPTS
